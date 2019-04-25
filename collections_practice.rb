5a1477a449191d5f6b19f55a682798af7987df24
Rick Rosetti <rick.rosetti@gmail.com>
2019-04-25 15:11:24 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -31,7 +31,7 @@ end
 
 def reverse_array(array)
   new_index_order =[]
-  new_index_order = (array.length-1..0).to_a
+  new_index_order = Range.new(array.length-1,0).to_a
   binding.pry
   new_output = []
   new_index_order.each do |i|
